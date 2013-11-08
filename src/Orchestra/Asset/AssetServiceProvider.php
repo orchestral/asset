01bc2098ce9b4c150c09ab728c078f58d6ceb230
crynobone <crynobone@gmail.com>
2013-11-08 16:11:40 +0800

    Remove alias from service provider
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Orchestra/Asset/AssetServiceProvider.php
+++ src/Orchestra/Asset/AssetServiceProvider.php
@@ -1,7 +1,6 @@
 <?php namespace Orchestra\Asset;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Foundation\AliasLoader;
 
 class AssetServiceProvider extends ServiceProvider
 {
@@ -21,11 +20,6 @@ class AssetServiceProvider extends ServiceProvider
     {
         $this->app['orchestra.asset'] = $this->app->share(function ($app) {
             return new Environment($app);
-        });
-
-        $this->app->booting(function () {
-            $loader = AliasLoader::getInstance();
-            $loader->alias('Orchestra\Asset', 'Orchestra\Support\Facades\Asset');
         });
     }
 
