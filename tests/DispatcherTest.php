e1458fd118330afe58b3f6cdd62eba73a424f46f
crynobone <crynobone@gmail.com>
2014-05-03 22:07:16 +0800

    Improve testcase.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- tests/DispatcherTest.php
+++ tests/DispatcherTest.php
@@ -14,7 +14,7 @@ class DispatcherTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * Test Orchesta\Asset\Dispatcher::run() metho.
+     * Test Orchesta\Asset\Dispatcher::run() method.
      *
      * @test
      */
@@ -50,6 +50,57 @@ class DispatcherTest extends \PHPUnit_Framework_TestCase
                 ->andReturn('foo')
             ->shouldReceive('script')->twice()
                 ->with('//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js', m::any())
+                ->andReturn('jquery');
+        $resolver->shouldReceive('arrange')->twice()->with($script)->andReturn($script);
+
+        $stub = new Dispatcher($files, $html, $resolver, $path);
+
+        $stub->addVersioning();
+
+        $this->assertEquals('jqueryfoo', $stub->run('script', $assets));
+        $this->assertEquals('', $stub->run('style', $assets));
+
+        $stub->removeVersioning();
+
+        $this->assertEquals('jqueryfoo', $stub->run('script', $assets));
+    }
+
+    /**
+     * Test Orchesta\Asset\Dispatcher::run() method using remote path.
+     *
+     * @test
+     */
+    public function testRunMethodUsingRemotePath()
+    {
+        $files = m::mock('\Illuminate\Filesystem\Filesystem');
+        $html = m::mock('\Illuminate\Html\HtmlBuilder');
+        $resolver = m::mock('\Orchestra\Asset\DependencyResolver');
+        $path = '//cdn.foobar.com';
+
+        $script = array(
+            'jquery' => array(
+                'source'       => '//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js',
+                'dependencies' => array(),
+                'attributes'   => array(),
+            ),
+            'foo' => array(
+                'source'       => 'foo.js',
+                'dependencies' => array(),
+                'attributes'   => array(),
+            ),
+            'foobar' => null,
+        );
+
+        $assets = array(
+            'script' => $script,
+            'style'  => array(),
+        );
+
+        $html->shouldReceive('script')->twice()
+                ->with('//cdn.foobar.com/foo.js', m::any())
+                ->andReturn('foo')
+            ->shouldReceive('script')->twice()
+                ->with('//cdn.foobar.com/ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js', m::any())
                 ->andReturn('jquery');
         $resolver->shouldReceive('arrange')->twice()->with($script)->andReturn($script);
 
