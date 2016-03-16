070fd42c3aa02fe2a10aeaa76825f79a04df49b3
crynobone <crynobone@gmail.com>
2016-03-16 22:15:39 +0800

    Group with {}
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Dispatcher.php
+++ src/Dispatcher.php
@@ -119,7 +119,7 @@ class Dispatcher
 
         $asset['source'] = $this->getAssetSourceUrl($asset['source']);
 
-        return $this->html->$group($asset['source'], $asset['attributes']);
+        return $this->html->{$group}($asset['source'], $asset['attributes']);
     }
 
     /**
