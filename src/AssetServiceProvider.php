c5e2506ac1806d87b78f90c79d2c1ec4d88bbd1a
Mior Muhammad Zaki <crynobone@gmail.com>
2019-02-24 12:11:41 +0800

    Add RegistrableProvider.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/AssetServiceProvider.php
+++ src/AssetServiceProvider.php
@@ -5,8 +5,9 @@ namespace Orchestra\Asset;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Contracts\Support\RegistrableProvider;
 
-class AssetServiceProvider extends ServiceProvider implements DeferrableProvider
+class AssetServiceProvider extends ServiceProvider implements DeferrableProvider, RegistrableProvider
 {
     /**
      * Register the service provider.
@@ -27,7 +28,7 @@ class AssetServiceProvider extends ServiceProvider implements DeferrableProvider
      *
      * @return void
      */
-    protected function registerAsset()
+    protected function registerAsset(): void
     {
         $this->app->singleton('orchestra.asset', function (Application $app) {
             return new Factory($app->make('orchestra.asset.dispatcher'));
@@ -39,7 +40,7 @@ class AssetServiceProvider extends ServiceProvider implements DeferrableProvider
      *
      * @return void
      */
-    protected function registerDispatcher()
+    protected function registerDispatcher(): void
     {
         $this->app->singleton('orchestra.asset.dispatcher', function (Application $app) {
             return new Dispatcher(
@@ -56,7 +57,7 @@ class AssetServiceProvider extends ServiceProvider implements DeferrableProvider
      *
      * @return void
      */
-    protected function registerResolver()
+    protected function registerResolver(): void
     {
         $this->app->singleton('orchestra.asset.resolver', function () {
             return new DependencyResolver();
